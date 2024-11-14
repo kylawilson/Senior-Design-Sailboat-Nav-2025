83b9deccb961b124271a6d65e17ebcc233ccaed8
MGuacho <157783246+MGuacho@users.noreply.github.com>
2024-11-14 16:50:41 -0500

    Skeleton for GPS + Bluetooth of Main Raspberry Pi
    Implementation of Bluetooth and reading GPS for Raspberry pi. Still need to do peripheral but it is not that different.

---
+++ Triton/Central_Bluetooth+GPS.py
@@ -0,0 +1,246 @@
+
+import serial               #import serial pacakge
+from time import sleep
+#import webbrowser           #import package for opening link in browser
+import sys                  #import system package
+import aioble
+import bluetooth
+import asyncio
+import struct
+from sys import exit
+
+serve = bluetooth.UUID(0x1848)
+charc = bluetooth.UUID(0x2A6E)
+IAM = "Central"
+
+if IAM not in ['Peripheral','Central']:
+    print("IAM must be either Peripheral or Central")
+    exit()
+
+if IAM == "Central":
+    IAM_SENDING_TO = "Peripheral"
+else:
+    IAM_SENDING_TO = "Central"
+    
+    # Bluetooth parameters
+BLE_NAME = f"{IAM}"
+BLE_SVC_UUID = bluetooth.UUID(0x181A)
+BLE_CHARACTERISTIC_UUID = bluetooth.UUID(0x2A6E)
+BLE_APPEARANCE = 0x0300
+BLE_ADVERTISING_INTERVAL = 2000
+BLE_SCAN_LENGTH = 5000
+BLE_INTERVAL = 30000
+BLE_WINDOW = 30000
+
+#GPS parameters
+gpgga_info = "$GPGGA,"
+ser = serial.Serial ("/dev/ttyS0")
+GPGGA_buffer = 0
+NMEA_buff = 0
+lat_in_degrees = 0
+long_in_degrees = 0
+
+
+def GPS_Info():
+    global NMEA_buff
+    global lat_in_degrees
+    global long_in_degrees
+    nmea_time = []
+    nmea_latitude = []
+    nmea_longitude = []
+    nmea_time = NMEA_buff[0]                #extract time from GPGGA string
+    nmea_latitude = NMEA_buff[1]            #extract latitude from GPGGA string
+    nmea_longitude = NMEA_buff[3]           #extract longitude from GPGGA string
+    lat = float(nmea_latitude)              #convert string into float for calculation
+    longi = float(nmea_longitude)           #convertr string into float for calculation
+    lat_in_degrees = convert_to_degrees(lat)    #get latitude in degree decimal format
+    long_in_degrees = convert_to_degrees(longi) #get longitude in degree decimal format
+    
+#convert raw NMEA string into degree decimal format   
+def convert_to_degrees(raw_value):
+    decimal_value = raw_value/100.00
+    degrees = int(decimal_value)
+    mm_mmmm = (decimal_value - int(decimal_value))/0.6
+    position = degrees + mm_mmmm
+    position = "%.4f" %(position)
+    return position
+
+def encode_message(message):
+    """ Encode a message to bytes """
+    return message.encode('utf-8')
+
+def decode_message(message):
+    """ Decode a message from bytes """
+    return message.decode('utf-8')
+
+async def send_data_task(connection, characteristic):
+    """ Send data to the connected device """
+    while True:
+        if not connection:
+            print("error - no connection in send data")
+            continue
+
+        if not characteristic:
+            print("error no characteristic provided in send data")
+            continue
+
+        message = f"("lat in degrees:", lat_in_degrees," long in degree: ", long_in_degrees, '\n')"
+
+        try:
+            msg = encode_message(message)
+            characteristic.write(msg)
+
+            await asyncio.sleep(0.5)
+            response = decode_message(characteristic.read())
+
+        except Exception as e:
+            print(f"writing error {e}")
+            continue
+
+        await asyncio.sleep(0.5)
+        
+async def ble_scan():
+    """ Scan for a BLE device with the matching service UUID """
+
+    print(f"Scanning for BLE Beacon named {BLE_NAME}...")
+
+    async with aioble.scan(5000, interval_us=30000, window_us=30000, active=True) as scanner:
+        async for result in scanner:
+            if result.name() == IAM_SENDING_TO and BLE_SVC_UUID in result.services():
+                print(f"found {result.name()} with service uuid {BLE_SVC_UUID}")
+                return result
+    return None
+
+
+async def receive_data_task(characteristic):
+    """ Receive data from the connected device """
+    global message_count
+    while True:
+        try:
+            data = await characteristic.read()
+
+            if data:
+                print(f"{IAM} received: {decode_message(data)}, count: {message_count}")
+                await characteristic.write(encode_message("Got it"))
+                await asyncio.sleep(0.5)
+
+            message_count += 1
+
+        except asyncio.TimeoutError:
+            print("Timeout waiting for data in {ble_name}.")
+            break
+        except Exception as e:
+            print(f"Error receiving data: {e}")
+            break
+
+
+async def run_peripheral_mode():
+    """ Run the peripheral mode """
+
+    # Set up the Bluetooth service and characteristic
+    ble_service = aioble.Service(BLE_SVC_UUID)
+    characteristic = aioble.Characteristic(
+        ble_service,
+        BLE_CHARACTERISTIC_UUID,
+        read=True,
+        notify=True,
+        write=True,
+        capture=True,
+    )
+    aioble.register_services(ble_service)
+
+    print(f"{BLE_NAME} starting to advertise")
+
+    while True:
+        async with await aioble.advertise(
+            BLE_ADVERTISING_INTERVAL,
+            name=BLE_NAME,
+            services=[BLE_SVC_UUID],
+            appearance=BLE_APPEARANCE) as connection:
+            print(f"{BLE_NAME} connected to another device: {connection.device}")
+
+            tasks = [
+                asyncio.create_task(send_data_task(connection, characteristic)),
+            ]
+            await asyncio.gather(*tasks)
+            print(f"{IAM} disconnected")
+            break
+
+async def run_central_mode():
+    """ Run the central mode """
+
+    # Start scanning for a device with the matching service UUID
+    while True:
+        device = await ble_scan()
+
+        if device is None:
+            continue
+        print(f"device is: {device}, name is {device.name()}")
+
+        try:
+            print(f"Connecting to {device.name()}")
+            connection = await device.device.connect()
+
+        except asyncio.TimeoutError:
+            print("Timeout during connection")
+            continue
+
+        print(f"{IAM} connected to {connection}")
+
+        # Discover services
+        async with connection:
+            try:
+                service = await connection.service(BLE_SVC_UUID)
+                characteristic = await service.characteristic(BLE_CHARACTERISTIC_UUID)
+            except (asyncio.TimeoutError, AttributeError):
+                print("Timed out discovering services/characteristics")
+                continue
+            except Exception as e:
+                print(f"Error discovering services {e}")
+                await connection.disconnect()
+                continue
+
+            tasks = [
+                asyncio.create_task(receive_data_task(characteristic)),
+            ]
+            await asyncio.gather(*tasks)
+
+            await connection.disconnected()
+            print(f"{BLE_NAME} disconnected from {device.name()}")
+            break
+
+async def main():
+    while True:
+        received_data = (str)(ser.readline())                   
+        GPGGA_data_available = received_data.find(gpgga_info)                   
+        if (GPGGA_data_available>0):
+            GPGGA_buffer = received_data.split("$GPGGA,",1)[1]  #store data coming after "$GPGGA," string 
+            NMEA_buff = (GPGGA_buffer.split(','))               #store comma separated data in buffer
+            GPS_Info()
+        if IAM == "Central":
+            tasks = [
+                asyncio.create_task(run_central_mode()),
+            ]
+        else:
+            tasks = [
+                asyncio.create_task(run_peripheral_mode()),
+            ]
+
+        await asyncio.gather(*tasks)
+
+asyncio.run(main())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
