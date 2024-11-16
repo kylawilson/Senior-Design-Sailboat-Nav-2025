9e6e01c920d2c745c204226a9df4ff7b1d93f97f
Kconn7 <114377119+Kconn7@users.noreply.github.com>
2024-11-16 15:59:56 -0500

    Add files via upload

---
+++ OAK-D/spatial_calculator_multi_roi.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+
+import cv2
+import depthai as dai
+import math
+import numpy as np
+from datetime import datetime
+
+# Create pipeline
+pipeline = dai.Pipeline()
+
+# Define sources and outputs
+monoLeft = pipeline.create(dai.node.MonoCamera)
+monoRight = pipeline.create(dai.node.MonoCamera)
+stereo = pipeline.create(dai.node.StereoDepth)
+spatialLocationCalculator = pipeline.create(dai.node.SpatialLocationCalculator)
+
+camRgb = pipeline.create(dai.node.ColorCamera)
+xoutRgb = pipeline.create(dai.node.ColorCamera)
+
+xoutDepth = pipeline.create(dai.node.XLinkOut)
+xoutSpatialData = pipeline.create(dai.node.XLinkOut)
+xinSpatialCalcConfig = pipeline.create(dai.node.XLinkIn)
+
+xoutDepth.setStreamName("depth")
+xoutSpatialData.setStreamName("spatialData")
+xinSpatialCalcConfig.setStreamName("spatialCalcConfig")
+
+# Properties
+monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoLeft.setCamera("left")
+monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoRight.setCamera("right")
+
+stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+stereo.setLeftRightCheck(True)
+stereo.setSubpixel(True)
+spatialLocationCalculator.inputConfig.setWaitForMessage(False)
+
+size = 10 #size by size 
+scale = 1/size
+
+
+# Create 10 ROIs
+for i in range(size):
+    for j in range(size):
+        config = dai.SpatialLocationCalculatorConfigData()
+        config.depthThresholds.lowerThreshold = 100
+        config.depthThresholds.upperThreshold = 12000
+        config.roi = dai.Rect(dai.Point2f((i)*scale, j*scale), dai.Point2f((i+1)*scale, (j+1)*scale))
+        spatialLocationCalculator.initialConfig.addROI(config)
+
+
+# Linking
+monoLeft.out.link(stereo.left)
+monoRight.out.link(stereo.right)
+
+spatialLocationCalculator.passthroughDepth.link(xoutDepth.input)
+stereo.depth.link(spatialLocationCalculator.inputDepth)
+
+spatialLocationCalculator.out.link(xoutSpatialData.input)
+xinSpatialCalcConfig.out.link(spatialLocationCalculator.inputConfig)
+
+# Connect to device and start pipeline
+with dai.Device(pipeline) as device:
+    device.setIrLaserDotProjectorBrightness(1000)
+
+    # Output queue will be used to get the depth frames from the outputs defined above
+    depthQueue = device.getOutputQueue(name="depth", maxSize=4, blocking=False)
+    spatialCalcQueue = device.getOutputQueue(name="spatialData", maxSize=4, blocking=False)
+    color = (0,40,200) #RGB color of ROI area
+    fontType = cv2.FONT_HERSHEY_TRIPLEX
+
+    target_distance = 5000
+    twom = (200,0,0)
+    onem = (0,0,200)
+    default_color = (0,200,40)
+    with open("roi_distances.txt", "a") as file:  # Open the file in append mode
+        while True:
+            inDepth = depthQueue.get() # Blocking call, will wait until a new data has arrived
+
+            depthFrame = inDepth.getFrame() # depthFrame values are in millimeters
+
+            depth_downscaled = depthFrame[::4]
+            if np.all(depth_downscaled == 0):
+                min_depth = 0  # Set a default minimum depth value when all elements are zero
+            else:
+                min_depth = np.percentile(depth_downscaled[depth_downscaled != 0], 1)
+            max_depth = np.percentile(depth_downscaled, 99)
+            depthFrameColor = np.interp(depthFrame, (min_depth, max_depth), (0, 255)).astype(np.uint8)
+            depthFrameColor = cv2.applyColorMap(depthFrameColor, cv2.COLORMAP_HOT)
+
+            spatialData = spatialCalcQueue.get().getSpatialLocations()
+            for depthData in spatialData:
+                roi = depthData.config.roi
+                roi = roi.denormalize(width=depthFrameColor.shape[1], height=depthFrameColor.shape[0])
+
+                xmin = int(roi.topLeft().x)
+                ymin = int(roi.topLeft().y)
+                xmax = int(roi.bottomRight().x)
+                ymax = int(roi.bottomRight().y)
+                grid_x = int(roi.topLeft().x)
+                grid_y = int(roi.topLeft().y)
+
+                coords = depthData.spatialCoordinates
+                distance = math.sqrt(coords.x ** 2 + coords.y ** 2 + coords.z ** 2)
+
+                if distance <= 2000 and distance >= 700:
+                    color = twom
+
+                elif distance <= 700 and distance != 0:
+                    color = onem
+                    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    file.write(f"{timestamp} - ROI {roi}: Distance {distance/1000:.2f} meters\n")
+                    print(f"ROI at grid position ({int(xmin)/64}, {int(ymin)/40}) is too close!: {distance / 1000:.1f}m")
+
+                else:
+                    color = default_color
+
+                text = "({},{},{:.1f}m)".format(int(grid_x/64), int(grid_y/40),distance/1000)
+                cv2.rectangle(depthFrameColor, (xmin, ymin), (xmax, ymax), color, thickness=2)
+                cv2.putText(depthFrameColor, "{:.1f}m".format(distance/1000), (xmin + 10, ymin + 20), fontType, 0.3, color)
+            # Show the frame
+            black_screen = np.zeros_like(depthFrameColor)
+            #cv2.imshow("depth", black_screen)
+            cv2.imshow("depth", depthFrameColor)
+            #cv2.resizeWindow("depth", 640, 480)
+
+            if cv2.waitKey(1) == ord('q'):
+                break
