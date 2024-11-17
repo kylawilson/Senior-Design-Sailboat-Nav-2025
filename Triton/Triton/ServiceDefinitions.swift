c27ecfa192a8877d766a001d7c4e3c8ff2f96ab5
kylawilson <kylaw@bu.edu>
2024-11-17 15:28:38 -0500

    change some stuff in the app: UUID, connecting on peripheral

--- Triton/Triton/ServiceDefinitions.swift
+++ Triton/Triton/ServiceDefinitions.swift
@@ -19,8 +19,9 @@ enum ConnectionStatus {
 
 struct TransferService {
     //may need to add more service IDs - one for each device we connect to
+    
+    static let tritonAdvertisingServiceUUID = CBUUID(string: "A3A3") //advertisement UUID
     static let tritonServiceUUID1 = CBUUID(string: "ec2ce16f-f774-4c1f-b3dd-a56b64bc9037") //custom service UUID
-    static let tritonServiceUUID2 = CBUUID(string: "180D") //custom service UUID
     static let tritonCharacteristicUUID = CBUUID(string: "842c3d51-9599-4c9c-aa41-15a28cb48bce") //I think we need one for each function the device performs
 }
 
