9e0d5688379278badd0bdf90f72126574baed8f7
kylawilson <kylaw@bu.edu>
2025-02-25 17:41:10 -0500

    clean up code

--- Triton/Triton/ImageConversion.swift
+++ Triton/Triton/ImageConversion.swift
@@ -10,34 +10,6 @@ import SwiftUI
 
 //https://stackoverflow.com/questions/51557553/decoding-base64-image-in-swift
 
-
-let filePath = Bundle.main.path(forResource: "img_serialized", ofType: "txt")
-
-func readData() -> UIImage? {
-    var imageEncoded: UIImage? = nil
-    do {
-        // Get the saved data
-        let savedData = try Data(contentsOf: URL(fileURLWithPath: filePath!))
-        
-        // Convert the data back into a string
-        if let savedString = String(data: savedData, encoding: .utf8) {
-            // Split the content by the delimiter
-            let parts = savedString.components(separatedBy: "\n|||IMAGE_END|||\n")
-            
-            for (_, part) in parts.enumerated() {
-                let trimmedPart = part.trimmingCharacters(in: .whitespacesAndNewlines) // Remove unnecessary spaces/newlines
-                if !trimmedPart.isEmpty { // Ensure we don't save empty parts
-                    imageEncoded = base64Convert(base64String: trimmedPart)
-                    return imageEncoded
-                }
-            }
-        }
-    } catch {
-        print("Unable to read the file: \(error)")
-    }
-    return imageEncoded
-}
-
 func readImage(finalPhotoData: String) -> UIImage? {
     var imageEncoded: UIImage? = nil
     if !finalPhotoData.isEmpty { // Ensure we don't save empty parts
@@ -58,11 +30,6 @@ func base64Convert(base64String: String?) -> UIImage {
         }
         return decodedImage
     }
-}
-
-func dataToImage(_ data: Data?) -> UIImage? {
-    guard let data = data else { print("data return nil"); return nil }
-    return UIImage(data: data)
 }
 
 struct ImageView: View {
