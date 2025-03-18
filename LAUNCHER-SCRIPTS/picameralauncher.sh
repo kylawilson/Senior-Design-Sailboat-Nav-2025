cad06c9d4c3c042061f910cbf9fde44a849cfe9f
MGuacho <mguacho@bu.edu>
2025-03-18 19:12:35 -0400

    added script to launch picamera

---
+++ LAUNCHER-SCRIPTS/picameralauncher.sh
@@ -0,0 +1,12 @@
+
+#!/bin/sh
+#launcher.sh
+#to execute the camera function on boot
+pwd
+which python3
+cd /home/triton/Downloads/Senior-Design-Sailboat-Nav-2025/
+. bin/activate
+which python3
+cd stereopi
+pwd
+python3 /home/triton/Downloads/Senior-Design-Sailboat-Nav-2025/stereopi/6_dm_video.py
