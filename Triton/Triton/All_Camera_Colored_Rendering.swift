74a985f1772e9a36fbb06bd7463992f04fde0c11
diego21001 <113653432+diego21001@users.noreply.github.com>
2025-04-08 16:18:05 -0400

    Update All_Camera_Colored_Rendering.swift
    added rendering for other boats to be visible on the rendering

--- Triton/Triton/All_Camera_Colored_Rendering.swift
+++ Triton/Triton/All_Camera_Colored_Rendering.swift
@@ -1,16 +1,47 @@
+//
+//  ContentView.swift
+//  phone_test
+//
+//  Created by Diego D’Angelo-Cosme on 4/7/25.
+//
+//
+//  ContentView.swift
+//  phone_polar_map
+//
+//  Created by Diego D’Angelo-Cosme on 3/31/25.
+//
+
+//updated variables there may be bugs based on messing things up
+
 import SwiftUI
 
 struct PolarGridView: View {
     let rings = 5
     let lines = 8
     
+    
+    //let rawOAKDDistances: [CGFloat] = [15, 15, 15, 15, 15, 15, 15, 15, 15, 15] // Example values in range [0.5, 15]
+    //let rawOAKDDistances: [CGFloat] = [7.75, 7.75, 7.75, 7.75, 7.75, 7.75, 7.75, 7.75, 7.75, 7.75]
+    //let rawOAKDDistances: [CGFloat] = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+    //let rawLeftSPDistances: [CGFloat] = [8, 8, 8, 8, 8, 8, 8, 8, 8, 8] // Example values for yellow objects
+    //let rawRightSPObjectDistances2: [CGFloat] = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10] // Example values for yellow objects
+    //let rawOAKDDistances: [CGFloat] = [0.5, 2, 3.75, 5.6, 7, 10, 13, 14, 15, 15] // Example values in range [0.5, 15]
+    //let rawOAKDDistances: [CGFloat] = [14, 14, 14, 14, 14, 14, 14, 14, 14, 14]
+    //let rawLeftSPDistances: [CGFloat] = [14, 14, 14, 14, 14, 14, 14, 14, 14, 14]
+    //let rawRightSPDistances: [CGFloat] = [13, 13, 13, 13, 13, 13, 13, 13, 13, 13]
+    //let rawLeftSPDistances: [CGFloat] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    //let rawRightSPDistances: [CGFloat] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        
+    //let rawLeftSPDistances: [CGFloat] = [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9]
+    
+    //let otherboats: [(CGFloat, CGFloat)] = [(0.5, -45), (10, 0), (3, 32)]
+    
     //MARK: Use Below for Integration
     
     let rawOAKDDistances: [CGFloat]
     let rawLeftSPDistances: [CGFloat]
     let rawRightSPDistances: [CGFloat]
-    //important note: for the StereoPi, even if we only receive 1 value, we still need an array of at least 10
-    //of that value to create a solid bar for the depth
+    let otherboats: [(CGFloat, CGFloat)]
     
     
     
@@ -31,11 +62,13 @@ struct PolarGridView: View {
 
                     OAKDObjectView(distances: scaledOAKD, rawDistances: rawOAKDDistances, center: center, maxRadius: maxRadius)
 
-                    StereoPIZoneView(distances: scaledLeft, rawDistances: rawLeftSPDistances, center: center, maxRadius: maxRadius, startAngle: 225, color: .yellow)
+                    StereoPIZoneView(distances: scaledLeft, rawDistances: rawLeftSPDistances, center: center, maxRadius: maxRadius, startAngle: 225, color: .green)
 
-                    StereoPIZoneView(distances: scaledRight, rawDistances: rawRightSPDistances, center: center, maxRadius: maxRadius, startAngle: 45, color: .yellow)
+                    StereoPIZoneView(distances: scaledRight, rawDistances: rawRightSPDistances, center: center, maxRadius: maxRadius, startAngle: 45, color: .green)
 
                     BoatTriangleView(center: center)
+                    
+                    OtherBoatsView(boats: otherboats, center: center, scaleDistance: scaleDistance)
                 }
                 .frame(width: size, height: size)
                 .position(x: geometry.size.width / 2, y: geometry.size.height * 0.35)
@@ -70,8 +103,8 @@ struct ConcentricGridView: View {
             ForEach(0..<lines, id: \.self) { index in
                 Path { path in
                     let angle = Angle.degrees(Double(index) / Double(lines) * 360)
-                    let endX = center.x + CGFloat(cos(angle.radians)) * maxRadius
-                    let endY = center.y + CGFloat(sin(angle.radians)) * maxRadius
+                    let endX = center.x + cos(angle.radians) * maxRadius
+                    let endY = center.y + sin(angle.radians) * maxRadius
                     path.move(to: center)
                     path.addLine(to: CGPoint(x: endX, y: endY))
                 }
@@ -133,7 +166,6 @@ struct OAKDObjectView: View {
         }
     }
 }
-
 struct StereoPIZoneView: View {
     let distances: [CGFloat]
     let rawDistances: [CGFloat]
@@ -197,5 +229,112 @@ struct BoatTriangleView: View {
             }
             .stroke(Color.black, lineWidth: 2)
         )
+    }
+}
+
+
+/*
+struct OtherBoatsView: View {
+    let boats: [(distance: CGFloat, angle: CGFloat)]
+    let center: CGPoint
+    let scaleDistance: (CGFloat) -> CGFloat
+
+    var body: some View {
+        ForEach(Array(boats.enumerated()), id: \.offset) { _, boat in
+          
+            let adjustedAngle = 90 - boat.angle
+            let scaledDistance = scaleDistance(boat.distance)
+            let radians = Angle(degrees: Double(adjustedAngle)).radians
+            let position = CGPoint(
+                x: center.x + CGFloat(cos(radians)) * scaledDistance,
+                y: center.y - CGFloat(sin(radians)) * scaledDistance
+            )
+
+            Path { path in
+                let size: CGFloat = 18 // smaller than central triangle
+                path.move(to: CGPoint(x: position.x, y: position.y - size / 2))
+                path.addLine(to: CGPoint(x: position.x - size / 2, y: position.y + size / 2))
+                path.addLine(to: CGPoint(x: position.x + size / 2, y: position.y + size / 2))
+                path.closeSubpath()
+            }
+            .fill(Color.purple)
+            .overlay(
+                Path { path in
+                    let size: CGFloat = 18
+                    path.move(to: CGPoint(x: position.x, y: position.y - size / 2))
+                    path.addLine(to: CGPoint(x: position.x - size / 2, y: position.y + size / 2))
+                    path.addLine(to: CGPoint(x: position.x + size / 2, y: position.y + size / 2))
+                    path.closeSubpath()
+                }
+                .stroke(Color.black, lineWidth: 2)
+            )
+            
+            Text("(\(String(format: "%.1f", boat.distance)), \(Int(boat.angle)))")
+                .font(.caption2)
+                .foregroundColor(.black)
+                .position(x: position.x, y: position.y - 25)
+        }
+    }
+}
+
+
+*/
+
+
+struct OtherBoatsView: View {
+    let boats: [(distance: CGFloat, angle: CGFloat)]
+    let center: CGPoint
+    let scaleDistance: (CGFloat) -> CGFloat
+
+    var body: some View {
+        let boatColors: [Color] = [.orange, .yellow, .purple]
+
+        ForEach(Array(boats.enumerated()), id: \.offset) { index, boat in
+            let color = boatColors[index % boatColors.count]
+            let adjustedAngle = 90 - boat.angle
+            let radians = Angle(degrees: Double(adjustedAngle)).radians
+            let scaledDistance = scaleDistance(boat.distance)
+
+            let position = CGPoint(
+                x: center.x + CGFloat(cos(radians)) * scaledDistance,
+                y: center.y - CGFloat(sin(radians)) * scaledDistance
+            )
+
+            ZStack {
+                // Triangle
+                Path { path in
+                    let size: CGFloat = 18
+                    path.move(to: CGPoint(x: position.x, y: position.y - size / 2))
+                    path.addLine(to: CGPoint(x: position.x - size / 2, y: position.y + size / 2))
+                    path.addLine(to: CGPoint(x: position.x + size / 2, y: position.y + size / 2))
+                    path.closeSubpath()
+                }
+                .fill(color)
+                .overlay(
+                    Path { path in
+                        let size: CGFloat = 18
+                        path.move(to: CGPoint(x: position.x, y: position.y - size / 2))
+                        path.addLine(to: CGPoint(x: position.x - size / 2, y: position.y + size / 2))
+                        path.addLine(to: CGPoint(x: position.x + size / 2, y: position.y + size / 2))
+                        path.closeSubpath()
+                    }
+                    .stroke(Color.black, lineWidth: 2)
+                )
+/*
+                // Label
+                Text("(\(String(format: "%.1f", boat.distance)), \(Int(adjustedAngle)))")
+                    .font(.caption2)
+                    .foregroundColor(.black)
+                    .position(x: position.x, y: position.y - 25)
+ */
+            }
+        }
+    }
+}
+
+
+struct PolarGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
